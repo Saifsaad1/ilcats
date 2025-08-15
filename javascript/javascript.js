49466d22d578c182875edeab56d0888e6b4b84f2
Saif Saad <saifsaad65a@gmail.com>
2025-08-16 01:40:29 +0300

    Add all project files

---
+++ javascript/javascript.js
@@ -0,0 +1,7 @@
+const   menuIcon = document.querySelector('.burger-menu');
+const navbar = document.querySelector('#nav');
+
+menuIcon.addEventListener('click',  () => {
+navbar.classList.toggle("change");
+})
+    
